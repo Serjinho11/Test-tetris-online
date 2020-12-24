e54baae387ebbf00e3d76935ce366e62371659e7
Serjinho11 <44323117+Serjinho11@users.noreply.github.com>
2020-12-24 13:47:28 +0200

    Add files via upload

---
+++ Chat_s.rar (Unzipped Files)/Chat_s/Chat_s/Server.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Chat_s
+{
+    partial class fServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbServerDate = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(334, 315);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tbServerDate
+            // 
+            this.tbServerDate.Location = new System.Drawing.Point(12, 347);
+            this.tbServerDate.Name = "tbServerDate";
+            this.tbServerDate.Size = new System.Drawing.Size(240, 20);
+            this.tbServerDate.TabIndex = 1;
+            this.tbServerDate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbServerDate_KeyPress);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(269, 347);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // fServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 413);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbServerDate);
+            this.Controls.Add(this.textBox1);
+            this.Name = "fServer";
+            this.Text = "Chat-Server";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbServerDate;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Chat_s.rar (Unzipped Files)/Chat_s/Chat_s/Server.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat_s
+{
+    public partial class fServer : Form
+    {
+        public TcpListener server;
+        public String dateServer;
+     
+        private static fServer serverForm;
+        Thread t;
+        bool workThread;
+        NetworkStream streamServer;
+
+        public fServer()
+        {
+            InitializeComponent();
+            server = new TcpListener(System.Net.IPAddress.Any,3000);
+            server.Start();
+            t = new Thread(new ThreadStart(Asculta_Server));
+            workThread = true;
+           
+            t.Start();
+            serverForm = this;
+        }
+        public void Asculta_Server()
+        {
+
+            while (workThread)
+            {
+                Socket socketServer = server.AcceptSocket();
+                try
+                {
+                    streamServer = new NetworkStream(socketServer);
+                    StreamReader citireServer = new StreamReader(streamServer);
+                   
+                    while (workThread)
+                    {
+
+                        string dateServer= citireServer.ReadLine();
+                        //char temp;
+                        //    do {
+                        //    temp = (char)citireServer.Read();
+                        //    dateServer += temp;
+                        //} while (!citireServer.EndOfStream);
+
+
+                        if (dateServer == null) break;//primesc nimic - clientul a plecat
+                        if (dateServer == "#Gata") //ca sa pot sa inchid serverul
+                            workThread = false;
+                        MethodInvoker m = new MethodInvoker(() => serverForm.textBox1.Text += (socketServer.LocalEndPoint + ": " + dateServer + Environment.NewLine));
+                        serverForm.textBox1.Invoke(m);
+                    }
+                    streamServer.Close();
+                }
+                catch (Exception e)
+                {
+#if LOG
+                    Console.WriteLine(e.Message);
+#endif
+                }
+                socketServer.Close();
+            }
+
+        }
+       
+
+       
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            workThread = false;
+            streamServer.Close();
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                StreamWriter scriere = new StreamWriter(streamServer);
+                scriere.AutoFlush = true; // enable automatic flushing
+                scriere.WriteLine(tbServerDate.Text);
+                textBox1.Text += "Server: " + tbServerDate.Text + Environment.NewLine;
+                tbServerDate.Clear();
+                // s_text.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+                //  client.Close();
+            } 
+        }
+
+        private void tbServerDate_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                btnSend_Click(sender, e);
+            }
+        }
+
+       
+    }
+}
